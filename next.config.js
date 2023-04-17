38501525529ccbc8e9a51d40463d4a40003b8b44
Better Futures <betterfuture032@gmail.com>
2023-04-16 20:35:56 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'randomuser.me'
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ sanitybackend/schemas/post.js
@@ -0,0 +1,53 @@
+export default {
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [
+        {
+        name: 'caption',
+        title: 'Caption',
+        type: 'string',
+    },
+    {
+        name: 'video',
+        title: 'Video',
+        type: 'file',
+        option: {
+            hotspot: true,
+        },
+    },  
+    
+    {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+    },
+    {
+        name: 'postedBy',
+        title: 'Posted By',
+        type: 'postedBy',
+    },
+    {
+        name: 'likes',
+        title: 'Likes',
+        type: 'array',
+        of: [
+         {
+            type: 'reference',
+            to: [{type: 'user'}],
+            },
+        ],
+    },
+    {
+        name: 'comments',
+        title: 'Comments',
+        type: 'array',
+        of: [{type: 'comment'}]
+    },
+    {
+        name: 'topic',
+        title: 'Topic',
+        type: 'string',
+    },
+],
+}
